46c31a0c11ab4c8d157c3607ffdd8d4ad5037091
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2022-08-08 12:13:02 +0900

    Backend-Commit-1

---
+++ Calorie Tracker App/server/routes/entries.go
@@ -0,0 +1,82 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var entryCollection *mongo.Collection = OpenCollection(Client, "calories")
+
+func AddEntry(c *gin.Context) {
+
+}
+
+func GetEntries(c *gin.Context) {
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var entries []bson.M
+
+	cursor, err := entryCollection.Find(ctx, bson.M{})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	if err = cursor.All(ctx, &entries); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	fmt.Println(entries)
+	c.JSON(http.StatusOK, entries)
+}
+
+func GetEntriesByIngredient(c *gin.Context) {
+
+}
+
+func GetEntryById(c *gin.Context) {
+	EntryID := c.Params.ByName("id")
+	docID, _ := primitive.ObjectIDFromHex(EntryID)
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	var entry bson.M
+	if err := entryCollection.FindOne(ctx, bson.M{"_id": EntryID}).Decode(&entry); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	fmt.Println(entry)
+}
+
+func UpdateIngredient(c *gin.Context) {
+
+}
+
+func UpdateEntry(c *gin.Context) {
+
+}
+
+func DeleteEntry(c *gin.Context) {
+	entryID := c.Params.ByName(("id"))
+	docID, _ := primitive.ObjectIDFromHex(entryID)
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	result, err := entryCollection.DeleteOne(ctx, bson.M{"_id": docID})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+	defer cancel()
+	c.JSON(http.StatusOK, result.DeletedCount)
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Golang
+Frontend
+  React 
+    simple functional component
+
+Backend
+  Go
+    Router
+    MongoDB packages
+DB
+  MongoDB
+
+Package Intialization
+  github.com/pradytpk/go-react-calorie/routes
+
+
+VS code Extension
+  tabnine- autocomplete 
